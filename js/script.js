a8958d149ab5b53a40b25784dbac4f2d52828fcf
Ethan <ewalker15@yahoo.com>
2015-06-11 20:37:13 -0500

    Moved FizzBuzz logic to function and set up loop

--- js/script.js
+++ js/script.js
@@ -5,15 +5,40 @@ $( document ).ready(function() {
   // current iterator value is divisible
   // and  appends correct result to the 
   // list being displayed on page.
-    for(i = 1; i <= 100; i++)
+  var MAX = 0;
+  var myList = document.getElementById("list");
+  while(MAX != -1)
+  {
+    MAX = parseInt(prompt("Please enter the integer you want to FizzBuzz to or -1 to exit loop ", "100"));
+
+    while(!MAX || (MAX < -1))
+    {
+     MAX = parseInt(prompt("That wasn't a valid integer. Please enter the number you want to FizzBuzz to or -1 to exit loop ", "100"));  
+    }
+        
+    runFizzBuzz(MAX);
+  
+
+  }
+  
+  
+  function runFizzBuzz(MAX){
+    if(MAX != -1)
+    {
+      while (myList.firstChild)
+      {
+        myList.removeChild(myList.firstChild);
+      }
+    }
+    for(i = 1; i <= MAX; i++)
     {
       if(i%3 == 0 && i%5 == 0)
       {
-        $("#list").append("<li>FizBuzz</li>");
+        $("#list").append("<li>FizzBuzz</li>");
       }
       else if(i%3 == 0)
       {
-        $("#list").append("<li>Fiz</li>");
+        $("#list").append("<li>Fizz</li>");
       }
       else if(i%5 == 0)
       {
@@ -24,4 +49,5 @@ $( document ).ready(function() {
         $("#list").append("<li>" + i + "</li>");
       }
     } 
+  }
 });
